e21b7e9a68de78d07f520754ad6a467715b697c6
areum59 <reddoll8859@gmail.com>
2024-06-05 17:12:31 +0900

    수정 버튼 블린에 따른 렌더링 수정
    버튼 클릭 이벤트에 따른 핸들링 추가

--- src/src/components/AddTodo.jsx
+++ src/src/components/AddTodo.jsx
@@ -1,21 +1,24 @@
-import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-export default function AddTodo({onAdd}) {
-    const [text, setText] = useState('');
+export default function AddTodo({ onAdd }) {
+    const [text, setText] = useState("");
 
     const handleChange = (e) => setText(e.target.value);
 
     const onSubmit = (e) => {
         e.preventDefault();
-        onAdd({id: uuidv4(), text, status: 'active'});
-        setText('');
-    }
+        if (text.trim().length === 0) {
+            return;
+        }
+        onAdd({ id: uuidv4(), text, status: "active" });
+        setText("");
+    };
 
     return (
         <form onSubmit={onSubmit}>
             <input type="text" value={text} onChange={handleChange} />
-            <button type='submit'>Add</button>
+            <button type="submit">Add</button>
         </form>
     );
 }
--- src/src/components/Todo.jsx
+++ src/src/components/Todo.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaTrashAlt, FaEdit } from "react-icons/fa";
 
-export default function Todo({ todo, onUpdate, onDelete }) {
+export default function Todo({ todo, onUpdate, onDelete, props }) {
     const { text, status } = todo;
+    const [updatedText, setUpdatedText] = useState(text);
+    const [isEditClicked, setIsEditClicked] = useState(false);
 
     const handleChange = (e) => {
         const status = e.target.checked ? "complete" : "active";
@@ -11,23 +13,60 @@ export default function Todo({ todo, onUpdate, onDelete }) {
 
     const handleDelete = () => onDelete(todo);
 
+    const handleOpenEdit = () => setIsEditClicked(true);
+
+    const handleEditText = () => setUpdatedText();
+
+    const handleSubmitEdited = () => {
+        if (updatedText === null) {
+            return;
+        }
+        isEditClicked(false);
+    };
+
+    const handleCancelEdit = () => {
+        setUpdatedText("");
+        setIsEditClicked(false);
+    };
+
     return (
         <li>
-            <input
-                type="checkbox"
-                name=""
-                id="checkbox"
-                checked={status === "complete"}
-                onChange={handleChange}
-            />
-            <label htmlFor="checkbox">{text}</label>
+            {isEditClicked === true ? (
+                <>
+                    <input type="text" value={updatedText} onChange={handleEditText} />
+                </>
+            ) : (
+                <>
+                    <input
+                        type="checkbox"
+                        name=""
+                        id="checkbox"
+                        checked={status === "complete"}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="checkbox">{text}</label>
+                </>
+            )}
 
-            <button type="button">
-                <FaEdit />
-            </button>
-            <button type="button" onClick={handleDelete}>
-                <FaTrashAlt />
-            </button>
+            {isEditClicked === true ? (
+                <>
+                    <button type="button" onClick={handleSubmitEdited}>
+                        확인
+                    </button>
+                    <button type="button" onClick={handleCancelEdit}>
+                        취소
+                    </button>
+                </>
+            ) : (
+                <>
+                    <button type="button" onClick={handleOpenEdit}>
+                        <FaEdit />
+                    </button>
+                    <button type="button" onClick={handleDelete}>
+                        <FaTrashAlt />
+                    </button>
+                </>
+            )}
         </li>
     );
 }
