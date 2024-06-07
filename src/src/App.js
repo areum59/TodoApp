c86d0a4d33931f4410f86fb09094da5321151d48
areum59 <reddoll8859@gmail.com>
2024-06-07 15:28:03 +0900

    static 구조 추가
    1. css 설정
    2. font 추가 및 적용

--- src/src/App.js
+++ src/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./App.css";
+import "./static/styles/common.css";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
 
